0a69d0895f22f7fd594ae286092ce8c11066cb5c
Jordan Stone <jordans2299@gmail.com>
2020-06-17 00:13:50 -0500

    first

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+      <Header />
+      <!-- v-on catches the emit -->
+      <AddTodo v-on:add-todo="addTodo"/>
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data(){
+    return {
+      todos:[]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id, res.data))
+      .catch(err =>console.log(err));
+    },
+    addTodo(newTodo){
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+      .then(res=>this.todos = [...this.todos,res.data])
+      .catch(error=>console.log(error))
+    //   fetch('https://jsonplaceholder.typicode.com/todos',{
+    //   method:'POST',
+    //   body: JSON.stringify(title,completed)
+    // })
+    // .then(response =>response.json())
+    // .then(data => this.todos = [...this.todos,data])
+    // .catch(error=>console.log(error));
+      //spread operator to add new todo to the list
+      
+    }
+  },
+  //vue function called after instance is created
+    created(){
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10',{
+      method:'GET'
+    })
+    .then(response =>response.json())
+    .then(data => this.todos = data)
+    .catch(error=>console.log(error));
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover{
+  background: #666;
+}
+</style>
